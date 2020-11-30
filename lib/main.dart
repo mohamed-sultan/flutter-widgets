e0dcf9bfaa307ad76f4ba6f76c4a655ba0240fa4
mohamed <mohamedhassansulatan@gmail.com>
2020-12-01 01:53:16 +0300

    row colum widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,14 +16,66 @@ class MyApp extends StatelessWidget {
     home: Scaffold(
       backgroundColor: Colors.green,
       body: SafeArea(
-        child: Container(
-          height: 200.0,
-          width: 300.0,
-          padding: EdgeInsets.all(30.0),
-          margin: EdgeInsets.only(left: 30.0),
-          color: Colors.white,
-          child: Text('cool'),
-        ),
+        child:Container(
+          child: Column(
+            children: [
+              Column(
+
+                crossAxisAlignment: CrossAxisAlignment.stretch,          children: <Widget>[
+                Container(
+                  height: 200.0,
+                  padding: EdgeInsets.all(30.0),
+                  color: Colors.white,
+                  child: Text('cool'),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  height: 200.0,
+                  padding: EdgeInsets.all(30.0),
+                  color: Colors.white,
+                  child: Text('cool'),
+                ),
+                SizedBox(height: 20.0),
+
+                Container(
+                  height: 200.0,
+                  padding: EdgeInsets.all(30.0),
+                  color: Colors.white,
+                  child: Text('cool'),
+                ),
+
+              ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 200.0,
+                    padding: EdgeInsets.all(30.0),
+                    color: Colors.red,
+                    child: Text('cool'),
+                  ),
+                  SizedBox(width: 20.0),
+                  Container(
+                    height: 200.0,
+                    padding: EdgeInsets.all(30.0),
+                    color: Colors.yellow,
+                    child: Text('cool'),
+                  ),
+                  SizedBox(width: 20.0),
+                  Container(
+                    height: 200.0,
+                    padding: EdgeInsets.all(30.0),
+                    color: Colors.yellow,
+                    child: Text('cool'),
+                  ),
+
+                ],
+              )
+            ],
+          ),
+        )
+
       ),
     ),
     );
